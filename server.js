470504ef233860488d3c9ab6232ba0f62693fc24
SabariRavi <30651882+SabariRavi@users.noreply.github.com>
2017-08-17 19:10:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ function createTemplate (data) {
                     <body>
                         <div class="container">
                         <div>
-                         <img src="/ui/Logo2.jpg"/>
+                         <img src="/ui/Logo_2.jpg"/>
                         </div>
                                 <div>
                                      <a href="/"> Home </a>
