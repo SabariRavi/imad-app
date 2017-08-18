224388dd42791d475879846d7276f474ced3d04d
SabariRavi <30651882+SabariRavi@users.noreply.github.com>
2017-08-18 19:16:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,22 @@
 $( "#sbt_btn" ).click(function() {
   var comment = $("#cmt_txt").val() ;
   //create a request object
-  
+  var xmlRequest= new XmlHttpRequest();
+     xmlRequest.onreadystatechange= function(){
+     if(xmlRequest.readystate === XmlHttpRequest.Done){
+         if(xmlRequest.status === 200){
+             var comments = xmlRequest.responseText;
+             comments=JSON.parse(comments);
+             var stringBuilder='';
+            for(var i=0; i < comments.length; i++)
+            {
+                 stringBuilder += "<li>" + comments[i] + "</li>";
+            }
+            
+            $("#lst").innerHTML(stringBuilder);
+         }
+     }
+ };
+  xmlRequest.open('GET', 'http://optimistwin.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+  xmlRequest.send(null);
 });
